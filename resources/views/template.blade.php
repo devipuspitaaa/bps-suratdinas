7571c11029257cbb7112d87d5c5fa28713f71366
devipuspitaaa <puspitasaridevi228@gmail.com>
2022-09-19 14:07:05 +0700

    template halaman dashboard

---
+++ resources/views/template.blade.php
@@ -0,0 +1,140 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <!-- Required meta tags -->
+  <meta charset="utf-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+  <title>Prodeskel</title>
+  <!-- plugins:css -->
+  <link rel="stylesheet" href="../assets/vendors/mdi/css/materialdesignicons.min.css">
+  <link rel="stylesheet" href="../assets/vendors/flag-icon-css/css/flag-icon.min.css">
+  <link rel="stylesheet" href="../assets/vendors/css/vendor.bundle.base.css">
+  <!-- endinject -->
+  <!-- Plugin css for this page -->
+  <link rel="stylesheet" href="../assets/vendors/jquery-bar-rating/css-stars.css" />
+  <link rel="stylesheet" href="../assets/vendors/font-awesome/css/font-awesome.min.css" />
+  <!-- End plugin css for this page -->
+  <!-- inject:css -->
+  <!-- endinject -->
+  <!-- Layout styles -->
+  <link rel="stylesheet" href="../assets/css/demo_1/style.css" />
+  <!-- End layout styles -->
+  <link rel="shortcut icon" href="../assets/images/favicon.png" />
+</head>
+
+<body>
+  <div class="container-scroller">
+    <div class="row p-0 m-0 proBanner" id="proBanner">
+      <div class="col-md-12 p-0 m-0">
+        <div class="card-body card-body-padding d-flex align-items-center justify-content-between">
+          <div class="ps-lg-1">
+            <div class="d-flex align-items-center justify-content-between">
+              <p class="mb-0 font-weight-medium me-3 buy-now-text">Free 24/7 customer support, updates, and more with this template!</p>
+              <a href="https://www.bootstrapdash.com/product/plus-admin-template/?utm_source=organic&utm_medium=banner&utm_campaign=buynow_demo" target="_blank" class="btn me-2 buy-now-btn border-0">Get Pro</a>
+            </div>
+          </div>
+          <div class="d-flex align-items-center justify-content-between">
+            <a href="https://www.bootstrapdash.com/product/plus-admin-template/"><i class="mdi mdi-home me-3 text-white"></i></a>
+            <button id="bannerClose" class="btn border-0 p-0">
+              <i class="mdi mdi-close text-white me-0"></i>
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+    <!-- partial:partials/_sidebar.html -->
+    <!-- navbar -->
+    <nav class="sidebar sidebar-offcanvas" id="sidebar">
+      <ul class="nav">
+        <li class="nav-item nav-profile border-bottom">
+          <a href="#" class="nav-link flex-column">
+            <div class="nav-profile-image">
+              <img src="../assets/images/faces/face1.jpg" alt="profile" />
+              <!--change to offline or busy as needed-->
+            </div>
+            <div class="nav-profile-text d-flex ms-0 mb-3 flex-column">
+              <span class="font-weight-semibold mb-1 mt-2 text-center">Ririn Mayangsari</span>
+            </div>
+          </a>
+        </li>
+        <li class="nav-item pt-3">
+          <a class="nav-link d-block" href="/">
+            <img class="sidebar-brand-logo" width="160px" src="../assets/images/prodeskel.png" alt="" />
+          </a>
+
+        </li>
+        <li class="pt-2 pb-1">
+          <span class="nav-item-head">Pages</span>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="/">
+            <i class="mdi mdi-compass-outline menu-icon"></i>
+            <span class="menu-title">Dashboard</span>
+          </a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" data-bs-toggle="collapse" href="#ui-basic" aria-expanded="false" aria-controls="ui-basic">
+            <i class="mdi mdi-crosshairs-gps menu-icon"></i>
+            <span class="menu-title">Potensi Prodeskel</span>
+            <i class="menu-arrow"></i>
+          </a>
+          <div class="collapse" id="ui-basic">
+            <ul class="nav flex-column sub-menu">
+              <li class="nav-item">
+                <a class="nav-link" href="/sdm">Sumber Daya Manusia</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="/pertanian">Pertanian</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="/ekonomi">Ekonomi</a>
+              </li>
+            </ul>
+          </div>
+        </li>
+      </ul>
+    </nav>
+    <!-- end navbar -->
+    <!-- partial -->
+    @yield('content')
+    <!-- page-body-wrapper ends -->
+    <!-- partial:partials/_footer.html -->
+    <footer class="footer">
+      <div class="d-sm-flex justify-content-center justify-content-sm-between">
+        <span class="text-muted text-center text-sm-left d-block d-sm-inline-block">Copyright © 2022 <a href="https://www.bootstrapdash.com/" target="_blank">BootstrapDash</a>. All rights reserved.</span>
+        <span class="float-none float-sm-right d-block mt-1 mt-sm-0 text-center">Hand-crafted & made with <i class="mdi mdi-heart text-danger"></i></span>
+      </div>
+    </footer>
+    <!-- partial -->
+  </div>
+  <!-- main-panel ends -->
+  </div>
+  </div>
+  <!-- container-scroller -->
+  <!-- plugins:js -->
+  <script src="../assets/vendors/js/vendor.bundle.base.js"></script>
+  <!-- endinject -->
+  <!-- Plugin js for this page -->
+  <script src="../assets/vendors/jquery-bar-rating/jquery.barrating.min.js"></script>
+  <script src="../assets/vendors/chart.js/Chart.min.js"></script>
+  <script src="../assets/vendors/flot/jquery.flot.js"></script>
+  <script src="../assets/vendors/flot/jquery.flot.resize.js"></script>
+  <script src="../assets/vendors/flot/jquery.flot.categories.js"></script>
+  <script src="../assets/vendors/flot/jquery.flot.fillbetween.js"></script>
+  <script src="../assets/vendors/flot/jquery.flot.stack.js"></script>
+  <script src="../assets/js/jquery.cookie.js" type="text/javascript"></script>
+  <!-- End plugin js for this page -->
+  <!-- inject:js -->
+  <script src="../assets/js/off-canvas.js"></script>
+  <script src="../assets/js/hoverable-collapse.js"></script>
+  <script src="../assets/js/misc.js"></script>
+  <script src="../assets/js/settings.js"></script>
+  <script src="../assets/js/todolist.js"></script>
+  <!-- endinject -->
+  <!-- Custom js for this page -->
+  <script src="../assets/js/dashboard.js"></script>
+  <!-- End custom js for this page -->
+</body>
+
+</html>
