b6238d53610888d8dcb775989e4a8cd4d421a4d0
SriKynanti <kynanti.ti@gmail.com>
2022-10-14 10:44:31 +0700

    Edit Sidebar, Footer, Header Tamplate

--- resources/views/template.blade.php
+++ resources/views/template.blade.php
@@ -39,31 +39,25 @@
             <!-- partial:partials/_navbar.html -->
             <nav class="navbar default-layout-navbar col-lg-12 col-12 p-0 fixed-top d-flex flex-row">
                 <div class="navbar-menu-wrapper d-flex align-items-stretch">
-                    <button class="navbar-toggler navbar-toggler align-self-center" type="button"
-                        data-toggle="minimize">
+                    <button class="navbar-toggler navbar-toggler align-self-center" type="button" data-toggle="minimize">
                         <span class="mdi mdi-chevron-double-left"></span>
                     </button>
                     <div class="text-center navbar-brand-wrapper d-flex align-items-center justify-content-center">
-                        <a class="navbar-brand brand-logo-mini" href="index.html"><img
-                                src="http://127.0.0.1:8000/assets/images/logo-mini.svg" alt="logo" /></a>
+                        <a class="navbar-brand brand-logo-mini" href="index.html"><img src="http://127.0.0.1:8000/assets/images/logo-mini.svg" alt="logo" /></a>
                     </div>
                     <ul class="navbar-nav">
                         <li class="nav-item dropdown">
-                            <a class="nav-link" id="messageDropdown" href="#" data-bs-toggle="dropdown"
-                                aria-expanded="false">
+                            <a class="nav-link" id="messageDropdown" href="#" data-bs-toggle="dropdown" aria-expanded="false">
                                 <i class="mdi mdi-email-outline"></i>
                             </a>
-                            <div class="dropdown-menu dropdown-menu-left navbar-dropdown preview-list"
-                                aria-labelledby="messageDropdown">
+                            <div class="dropdown-menu dropdown-menu-left navbar-dropdown preview-list" aria-labelledby="messageDropdown">
                                 <h6 class="p-3 mb-0 font-weight-semibold">Messages</h6>
                                 <div class="dropdown-divider"></div>
                                 <a class="dropdown-item preview-item">
                                     <div class="preview-thumbnail">
-                                        <img src="http://127.0.0.1:8000/assets/images/faces/face1.jpg" alt="image"
-                                            class="profile-pic">
+                                        <img src="http://127.0.0.1:8000/assets/images/faces/face1.jpg" alt="image" class="profile-pic">
                                     </div>
-                                    <div
-                                        class="preview-item-content d-flex align-items-start flex-column justify-content-center">
+                                    <div class="preview-item-content d-flex align-items-start flex-column justify-content-center">
                                         <h6 class="preview-subject ellipsis mb-1 font-weight-normal">Mark send you a
                                             message</h6>
                                         <p class="text-gray mb-0"> 1 Minutes ago </p>
@@ -72,11 +66,9 @@
                                 <div class="dropdown-divider"></div>
                                 <a class="dropdown-item preview-item">
                                     <div class="preview-thumbnail">
-                                        <img src="http://127.0.0.1:8000/assets/images/faces/face6.jpg" alt="image"
-                                            class="profile-pic">
+                                        <img src="http://127.0.0.1:8000/assets/images/faces/face6.jpg" alt="image" class="profile-pic">
                                     </div>
-                                    <div
-                                        class="preview-item-content d-flex align-items-start flex-column justify-content-center">
+                                    <div class="preview-item-content d-flex align-items-start flex-column justify-content-center">
                                         <h6 class="preview-subject ellipsis mb-1 font-weight-normal">Cregh send you a
                                             message</h6>
                                         <p class="text-gray mb-0"> 15 Minutes ago </p>
@@ -85,11 +77,9 @@
                                 <div class="dropdown-divider"></div>
                                 <a class="dropdown-item preview-item">
                                     <div class="preview-thumbnail">
-                                        <img src="http://127.0.0.1:8000/assets/images/faces/face7.jpg" alt="image"
-                                            class="profile-pic">
+                                        <img src="http://127.0.0.1:8000/assets/images/faces/face7.jpg" alt="image" class="profile-pic">
                                     </div>
-                                    <div
-                                        class="preview-item-content d-flex align-items-start flex-column justify-content-center">
+                                    <div class="preview-item-content d-flex align-items-start flex-column justify-content-center">
                                         <h6 class="preview-subject ellipsis mb-1 font-weight-normal">Profile picture
                                             updated</h6>
                                         <p class="text-gray mb-0"> 18 Minutes ago </p>
@@ -103,8 +93,7 @@
                             <a class="nav-link" id="notificationDropdown" href="#" data-bs-toggle="dropdown">
                                 <i class="mdi mdi-bell-outline"></i>
                             </a>
-                            <div class="dropdown-menu dropdown-menu-left navbar-dropdown preview-list"
-                                aria-labelledby="notificationDropdown">
+                            <div class="dropdown-menu dropdown-menu-left navbar-dropdown preview-list" aria-labelledby="notificationDropdown">
                                 <h6 class="px-3 py-3 font-weight-semibold mb-0">Notifications</h6>
                                 <div class="dropdown-divider"></div>
                                 <a class="dropdown-item preview-item">
@@ -113,8 +102,7 @@
                                             <i class="mdi mdi-calendar"></i>
                                         </div>
                                     </div>
-                                    <div
-                                        class="preview-item-content d-flex align-items-start flex-column justify-content-center">
+                                    <div class="preview-item-content d-flex align-items-start flex-column justify-content-center">
                                         <h6 class="preview-subject font-weight-normal mb-0">New order recieved</h6>
                                         <p class="text-gray ellipsis mb-0"> 45 sec ago </p>
                                     </div>
@@ -126,8 +114,7 @@
                                             <i class="mdi mdi-settings"></i>
                                         </div>
                                     </div>
-                                    <div
-                                        class="preview-item-content d-flex align-items-start flex-column justify-content-center">
+                                    <div class="preview-item-content d-flex align-items-start flex-column justify-content-center">
                                         <h6 class="preview-subject font-weight-normal mb-0">Server limit reached</h6>
                                         <p class="text-gray ellipsis mb-0"> 55 sec ago </p>
                                     </div>
@@ -139,8 +126,7 @@
                                             <i class="mdi mdi-link-variant"></i>
                                         </div>
                                     </div>
-                                    <div
-                                        class="preview-item-content d-flex align-items-start flex-column justify-content-center">
+                                    <div class="preview-item-content d-flex align-items-start flex-column justify-content-center">
                                         <h6 class="preview-subject font-weight-normal mb-0">Kevin karvelle</h6>
                                         <p class="text-gray ellipsis mb-0"> 11:09 PM </p>
                                     </div>
@@ -153,12 +139,11 @@
                     <ul class="navbar-nav navbar-nav-right">
                         <li class="nav-item nav-logout d-none d-lg-block">
                             <a class="nav-link" href="index.html">
-                                <i class="mdi mdi-home-circle"></i>
+                                <i class="mdi mdi-logout-variant"></i>
                             </a>
                         </li>
                     </ul>
-                    <button class="navbar-toggler navbar-toggler-right d-lg-none align-self-center" type="button"
-                        data-toggle="offcanvas">
+                    <button class="navbar-toggler navbar-toggler-right d-lg-none align-self-center" type="button" data-toggle="offcanvas">
                         <span class="mdi mdi-menu"></span>
                     </button>
                 </div>
