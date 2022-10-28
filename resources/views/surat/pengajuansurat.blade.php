7fa249e2a9791ead873a04813deed0dafac362d6
SriKynanti <kynanti.ti@gmail.com>
2022-10-28 22:52:34 +0700

    UI Pengajuan Surat

---
+++ resources/views/surat/pengajuansurat.blade.php
@@ -0,0 +1,22 @@
+@extends('template')
+@section('content')
+<div class="content-wrapper">
+    <div class="page-header">
+        <h3 class="page-title"> Pengajuan Surat </h3>
+    </div>
+    <div class="card">
+        <div class="card-body text-center">
+            <h4 class="card-title">Untuk Pengajuan Tanda Tangan Surat, Anda Dapat Mengakses Website dibawah ini:</h4>
+            <div class="row">
+                <div class="col-12">
+                    <div class="card card-inverse-success" id="context-menu-access">
+                        <div class="card-body">
+                            <a class="card-text" href="https://ettd.bps.go.id/" target="_blank"> ettdbps.go.id </p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
