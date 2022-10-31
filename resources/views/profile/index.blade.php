71c84e696d1ad283704b12fce80c35e2040f58f9
SriKynanti <kynanti.ti@gmail.com>
2022-10-31 16:29:53 +0700

    Update UI Profile

--- resources/views/profile/index.blade.php
+++ resources/views/profile/index.blade.php
@@ -14,18 +14,50 @@
                                 <img src="../../../assets/images/faces/face12.jpg" alt="profile" class="img-lg rounded-circle mb-3">
                             </div>
                         </div>
-                        <div class="col-lg-8">
+                        <div class="col-lg-4">
+
+                        </div>
+                        <div class="col-lg-4">
+
+                        </div>
+                        <div class="col-lg-4">
+
+                        </div>
+                        <div class="col-lg-4">
                             <div class="d-flex justify-content-between">
                                 <div>
                                     <h4>Username</h4>
                                     <p>Admin</p>
                                 </div>
+                            </div>
+                        </div>
+                        <div class="col-lg-4">
+                            <div class="d-flex justify-content-between">
                                 <div>
-                                    <h4>Nama Lengkap</h4>
+                                    <h4>Name</h4>
                                     <p>Administrator</p>
                                 </div>
                                 <div>
                                     <button class="btn btn-primary">Edit</button>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-lg-4">
+
+                        </div>
+                        <div class="col-lg-4">
+                            <div class="d-flex justify-content-between">
+                                <div>
+                                    <h4>E-mail</h4>
+                                    <p>Admin@gmail.com</p>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-lg-4">
+                            <div class="d-flex justify-content-between">
+                                <div>
+                                    <h4>NIP</h4>
+                                    <p>199909081020170901</p>
                                 </div>
                             </div>
                         </div>
