213b4647032aa17fba7dbec739600baaf05041e4
devipuspitaaa <puspitasaridevi228@gmail.com>
2022-11-09 13:04:54 +0700

    membuat model history surat

---
+++ app/Models/History_surat.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class History_surat extends Model
+{
+    use HasFactory;
+    protected $table = "history_surat";
+    public $timestamps = false;
+    protected $primaryKey = 'id';
+
+    protected $fillable = [
+        'id',
+        'file_historysurat',
+    ];
+
+    public function history()
+    {
+        return $this->belongsToMany(Surat_tugas::class);
+        return $this->belongsToMany(Surat_pengantar::class);
+        return $this->belongsToMany(Surat_undangan::class);
+    }
+}
